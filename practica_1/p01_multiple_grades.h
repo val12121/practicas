3420967aa0a963c3374dfdc20d604bfb3d661ca0
val12121 <alu0101548999@ull.edu.es>
2026-09-15 13:55:01 +0000

    Looking good

--- practica_1/p01_multiple_grades.h
+++ practica_1/p01_multiple_grades.h
@@ -14,20 +14,19 @@ class MultipleGrades {
       return false;
     }
     void Add(std::string alu, double grade) {
-      if (!Search(alu)) {
-        this->students_.insert(std::make_pair(alu, grade));
-      } else {
-        auto aux = this->students_.find(alu);
-        if (grade > aux->second) { aux->second = grade; }
-      }
+      this->students_.insert(std::make_pair(alu, grade));
     }
     friend std::ostream& operator<<(std::ostream& os, const MultipleGrades& multiple) {
       std::string previous_alu;
       for (const auto& pair : multiple.students_) {
         if (pair.first != previous_alu) {
-          os << "alu" << pair.first << ": " << pair.second << std::endl;  
+          if (!previous_alu.empty()) { os << std::endl; }
+          os << pair.first << ": ";
+          previous_alu = pair.first;
         }
+        os << pair.second << " ";                       
       }
+      os << std::endl;
       return os;
     }
 
